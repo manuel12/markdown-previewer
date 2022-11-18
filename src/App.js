3bb484ee16a09eb525d488c0f250c65fa98bf04e
Manuel <manuelpinedacabeza@gmail.com>
2022-11-18 17:56:58 +0100

    Imported custom styles, updated class names.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { marked } from "marked";
 import { useState } from "react";
+import "./custom-styles.css";
+import "./custom-colors.css";
 import "./App.css";
 
 const initialMarkdown =
@@ -22,10 +24,11 @@ const App = () => {
 
   return (
     <div className="App">
-      <div className="editor container-styles">
-        <div className="editor-header heading-styles">Editor</div>
+      <div className="editor custom-container bg-blue">
+        <div className="editor-header heading-depth bg-yellow">Editor</div>
         <textarea
           id="editor"
+          className="textarea"
           placeholder="Add your markdown here..."
           value={editorMarkdown}
           onChange={(e) => {
@@ -34,8 +37,8 @@ const App = () => {
           }}
         ></textarea>
       </div>
-      <div className="preview container-styles">
-        <div className="preview-header heading-styles">Previewer</div>
+      <div className="preview custom-container bg-blue">
+        <div className="preview-header heading-depth bg-yellow">Previewer</div>
         <div
           id="preview"
           className="preview-area"
